c73291a1885bf8ec1c0494cc1a202e6e594d1163
shub39 <cptnshubham39@gmail.com>
2024-07-09 10:00:51 +0530

    added dynamic expanded cards

--- app/src/main/java/com/shub39/rush/component/GroupedCard.kt
+++ app/src/main/java/com/shub39/rush/component/GroupedCard.kt
@@ -13,10 +13,6 @@ import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
