6a345633e17c8856e722b2a1c4ad5d05e1183ae8
shub39 <cptnshubham39@gmail.com>
2024-07-09 10:06:05 +0530

    Release 0.8.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.shub39.rush"
         minSdk = 29
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.7.0"
+        versionCode = 3
+        versionName = "0.8.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
