52e6548631a4c2b50c18400d29e7af73f80eefa9
shub39 <cptnshubham39@gmail.com>
2025-01-16 13:20:51 +0530

    :heavy_plus_sign: 2.4.3
    Signed-off-by: shub39 <cptnshubham39@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.shub39.rush"
         minSdk = 29
         targetSdk = 35
-        versionCode = 242
-        versionName = "2.4.2"
+        versionCode = 243
+        versionName = "2.4.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
