1947a660115810a9dd237daaeec00b4d756f9f32
shub39 <cptnshubham39@gmail.com>
2025-05-16 18:09:39 +0530

    :arrow_up: updated deps and bumped version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.shub39.rush"
         minSdk = 29
         targetSdk = 35
-        versionCode = 3100
-        versionName = "3.1.0"
+        versionCode = 3110
+        versionName = "3.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
