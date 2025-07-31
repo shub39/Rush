120d8f0708e62c2acf5544e1be1814d770a4a3ec
shub39 <cptnshubham39@gmail.com>
2025-07-31 06:54:29 +0530

    :wrench: fixes

--- app/src/play/java/com/shub39/rush/billing/PaywallPage.kt
+++ app/src/play/java/com/shub39/rush/billing/PaywallPage.kt
@@ -1,10 +1,19 @@
 package com.shub39.rush.billing
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.revenuecat.purchases.ui.revenuecatui.Paywall
 import com.revenuecat.purchases.ui.revenuecatui.PaywallOptions
 import com.revenuecat.purchases.ui.revenuecatui.customercenter.CustomerCenter
@@ -27,6 +36,22 @@ fun PaywallPage(
             Paywall(paywallOptions)
         } else {
             CustomerCenter(onDismiss = onDismissRequest)
+        }
+
+        IconButton(
+            onClick = onDismissRequest,
+            colors = IconButtonDefaults.iconButtonColors(
+                contentColor = Color(0xFF282828),
+                containerColor = Color(0xfff2e3b1)
+            ),
+            modifier = Modifier
+                .padding(vertical = 32.dp, horizontal = 16.dp)
+                .align(Alignment.TopEnd)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Close,
+                contentDescription = "Close"
+            )
         }
     }
 }
