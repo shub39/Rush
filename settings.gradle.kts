32a195a13102aa55fa254197e1a9b96f24a2af38
shub39 <cptnshubham39@gmail.com>
2026-01-27 17:29:01 +0530

    :wrench: foojay

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,9 +15,9 @@ pluginManagement {
     }
 }
 
-//plugins {
-//    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-//}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
