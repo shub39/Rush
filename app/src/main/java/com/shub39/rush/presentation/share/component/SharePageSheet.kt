27b86561ac419885b6ffea15e52084cee79719c5
shub39 <cptnshubham39@gmail.com>
2026-03-18 07:26:22 +0530

    :wrench: updated card edit sheet

--- app/src/main/java/com/shub39/rush/presentation/share/component/SharePageSheet.kt
+++ app/src/main/java/com/shub39/rush/presentation/share/component/SharePageSheet.kt
@@ -28,11 +28,13 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.SheetState
@@ -43,6 +45,7 @@ import androidx.compose.material3.rememberStandardBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -59,8 +62,13 @@ import com.shub39.rush.domain.enums.CardFit
 import com.shub39.rush.domain.enums.CardTheme
 import com.shub39.rush.domain.enums.CornerRadius
 import com.shub39.rush.domain.enums.Fonts
+import com.shub39.rush.presentation.components.ListItemCard
 import com.shub39.rush.presentation.components.ListSelect
 import com.shub39.rush.presentation.components.RushTheme
+import com.shub39.rush.presentation.endItemShape
+import com.shub39.rush.presentation.leadingItemShape
+import com.shub39.rush.presentation.listItemColors
+import com.shub39.rush.presentation.middleItemShape
 import com.shub39.rush.presentation.share.SharePageAction
 import com.shub39.rush.presentation.share.SharePageState
 import com.shub39.rush.presentation.toFontRes
@@ -85,154 +93,172 @@ fun SharePageSheet(
         sheetState = sheetState,
         modifier = modifier,
     ) {
-        HorizontalDivider()
         LazyColumn(
-            modifier = Modifier.fillMaxWidth().heightIn(max = 400.dp),
-            contentPadding = PaddingValues(16.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .heightIn(max = 500.dp)
+                .clip(RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp)),
+            contentPadding = PaddingValues(top = 16.dp, bottom = 60.dp),
+            verticalArrangement = Arrangement.spacedBy(2.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             item {
-                ListSelect(
-                    title = stringResource(R.string.card_theme),
-                    options = CardTheme.entries.toList(),
-                    selected = state.cardTheme,
-                    onSelectedChange = { onAction(SharePageAction.OnUpdateCardTheme(it)) },
-                    labelProvider = { Text(text = stringResource(it.toStringRes())) },
-                )
+                ListItemCard(shape = leadingItemShape()) {
+                    ListSelect(
+                        title = stringResource(R.string.card_theme),
+                        options = CardTheme.entries.toList(),
+                        selected = state.cardTheme,
+                        onSelectedChange = { onAction(SharePageAction.OnUpdateCardTheme(it)) },
+                        labelProvider = { Text(text = stringResource(it.toStringRes())) },
+                    )
+                }
             }
 
             item {
-                ListSelect(
-                    title = stringResource(R.string.card_color),
-                    options = CardColors.entries.toList(),
-                    selected = state.cardColors,
-                    onSelectedChange = { onAction(SharePageAction.OnUpdateCardColor(it)) },
-                    labelProvider = { Text(text = stringResource(it.toStringRes())) },
-                )
+                ListItemCard(shape = middleItemShape()) {
+                    ListSelect(
+                        title = stringResource(R.string.card_color),
+                        options = CardColors.entries.toList(),
+                        selected = state.cardColors,
+                        onSelectedChange = { onAction(SharePageAction.OnUpdateCardColor(it)) },
+                        labelProvider = { Text(text = stringResource(it.toStringRes())) },
+                    )
 
-                AnimatedVisibility(
-                    visible = state.cardColors == CardColors.CUSTOM,
-                    modifier = Modifier.fillMaxWidth(),
-                ) {
-                    Row(
-                        modifier =
-                            Modifier.padding(horizontal = 32.dp, vertical = 8.dp).fillMaxWidth(),
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    AnimatedVisibility(
+                        visible = state.cardColors == CardColors.CUSTOM,
+                        modifier = Modifier.fillMaxWidth(),
                     ) {
-                        IconButton(
-                            onClick = { onLaunchColorPicker("content") },
-                            colors =
-                                IconButtonDefaults.iconButtonColors(
-                                    containerColor = Color(state.cardContent),
-                                    contentColor = Color(state.cardBackground),
-                                ),
-                            modifier = Modifier.weight(1f),
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.spacedBy(8.dp),
                         ) {
-                            Icon(
-                                painter = painterResource(R.drawable.edit),
-                                contentDescription = "Select Color",
-                            )
-                        }
+                            IconButton(
+                                onClick = { onLaunchColorPicker("content") },
+                                colors =
+                                    IconButtonDefaults.iconButtonColors(
+                                        containerColor = Color(state.cardContent),
+                                        contentColor = Color(state.cardBackground),
+                                    ),
+                                modifier = Modifier.weight(1f),
+                            ) {
+                                Icon(
+                                    painter = painterResource(R.drawable.edit),
+                                    contentDescription = "Select Color",
+                                )
+                            }
 
-                        IconButton(
-                            onClick = { onLaunchColorPicker("background") },
-                            colors =
-                                IconButtonDefaults.iconButtonColors(
-                                    containerColor = Color(state.cardBackground),
-                                    contentColor = Color(state.cardContent),
-                                ),
-                            modifier = Modifier.weight(1f),
-                        ) {
-                            Icon(
-                                painter = painterResource(R.drawable.edit),
-                                contentDescription = "Select Color",
-                            )
+                            IconButton(
+                                onClick = { onLaunchColorPicker("background") },
+                                colors =
+                                    IconButtonDefaults.iconButtonColors(
+                                        containerColor = Color(state.cardBackground),
+                                        contentColor = Color(state.cardContent),
+                                    ),
+                                modifier = Modifier.weight(1f),
+                            ) {
+                                Icon(
+                                    painter = painterResource(R.drawable.edit),
+                                    contentDescription = "Select Color",
+                                )
+                            }
                         }
                     }
                 }
             }
 
             item {
-                ListSelect(
-                    title = stringResource(R.string.card_size),
-                    options = CardFit.entries.toList(),
-                    selected = state.cardFit,
-                    onSelectedChange = { onAction(SharePageAction.OnUpdateCardFit(it)) },
-                    labelProvider = { Text(text = stringResource(it.toStringRes())) },
-                )
-            }
-
-            item {
-                ListSelect(
-                    title = stringResource(R.string.card_corners),
-                    options = CornerRadius.entries.toList(),
-                    selected = state.cardRoundness,
-                    onSelectedChange = { onAction(SharePageAction.OnUpdateCardRoundness(it)) },
-                    labelProvider = { Text(text = stringResource(it.toStringRes())) },
-                )
-            }
-
-            item {
-                ListSelect(
-                    title = stringResource(R.string.album_art_shape),
-                    options = AlbumArtShape.entries.toList(),
-                    selected = state.albumArtShape,
-                    onSelectedChange = { onAction(SharePageAction.OnUpdateAlbumArtShape(it)) },
-                    labelProvider = {
-                        Box(
-                            modifier =
-                                Modifier.size(20.dp)
-                                    .background(
-                                        color = MaterialTheme.colorScheme.onSurface,
-                                        shape = it.toShape(),
-                                    )
-                        )
-                    },
-                )
-            }
-
-            item {
-                ListSelect(
-                    title = stringResource(R.string.card_font),
-                    options = Fonts.entries.toList(),
-                    selected = state.cardFont,
-                    onSelectedChange = { onAction(SharePageAction.OnUpdateCardFont(it)) },
-                    labelProvider = { font ->
-                        Text(
-                            text = font.toFullName(),
-                            fontFamily =
-                                font.toFontRes()?.let { FontFamily(Font(it)) } ?: FontFamily.Default,
-                        )
-                    },
-                )
-            }
-
-            item {
-                Row(
-                    modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp).fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.spacedBy(8.dp),
-                ) {
-                    Text(
-                        text = "Show Rush Branding",
-                        style = MaterialTheme.typography.titleLarge,
-                        maxLines = 1,
-                        modifier = Modifier.basicMarquee().weight(1f),
+                ListItemCard(shape = middleItemShape()) {
+                    ListSelect(
+                        title = stringResource(R.string.card_size),
+                        options = CardFit.entries.toList(),
+                        selected = state.cardFit,
+                        onSelectedChange = { onAction(SharePageAction.OnUpdateCardFit(it)) },
+                        labelProvider = { Text(text = stringResource(it.toStringRes())) },
                     )
+                }
+            }
 
-                    Switch(
-                        checked = state.rushBranding,
-                        onCheckedChange = {
-                            if (isProUser) {
-                                onAction(SharePageAction.OnToggleRushBranding(it))
-                            } else {
-                                onShowPaywall()
-                            }
+            item {
+                ListItemCard(shape = middleItemShape()) {
+                    ListSelect(
+                        title = stringResource(R.string.card_corners),
+                        options = CornerRadius.entries.toList(),
+                        selected = state.cardRoundness,
+                        onSelectedChange = { onAction(SharePageAction.OnUpdateCardRoundness(it)) },
+                        labelProvider = { Text(text = stringResource(it.toStringRes())) },
+                    )
+                }
+            }
+
+            item {
+                ListItemCard(shape = middleItemShape()) {
+                    ListSelect(
+                        title = stringResource(R.string.album_art_shape),
+                        options = AlbumArtShape.entries.toList(),
+                        selected = state.albumArtShape,
+                        onSelectedChange = { onAction(SharePageAction.OnUpdateAlbumArtShape(it)) },
+                        labelProvider = {
+                            Box(
+                                modifier =
+                                    Modifier
+                                        .size(20.dp)
+                                        .background(
+                                            color = MaterialTheme.colorScheme.onSurface,
+                                            shape = it.toShape(),
+                                        )
+                            )
                         },
                     )
                 }
+            }
+
+            item {
+                ListItemCard(shape = middleItemShape()) {
+                    ListSelect(
+                        title = stringResource(R.string.card_font),
+                        options = Fonts.entries.toList(),
+                        selected = state.cardFont,
+                        onSelectedChange = { onAction(SharePageAction.OnUpdateCardFont(it)) },
+                        labelProvider = { font ->
+                            Text(
+                                text = font.toFullName(),
+                                fontFamily =
+                                    font.toFontRes()?.let { FontFamily(Font(it)) }
+                                        ?: FontFamily.Default,
+                            )
+                        },
+                    )
+                }
+            }
+
+            item {
+                ListItem(
+                    colors = listItemColors(),
+                    modifier = Modifier.clip(endItemShape()),
+                    headlineContent = {
+                        Text(
+                            text = "Show Rush Branding",
+                            maxLines = 1,
+                            modifier = Modifier
+                                .basicMarquee()
+                                .weight(1f),
+                        )
+                    },
+                    trailingContent = {
+                        Switch(
+                            checked = state.rushBranding,
+                            onCheckedChange = {
+                                if (isProUser) {
+                                    onAction(SharePageAction.OnToggleRushBranding(it))
+                                } else {
+                                    onShowPaywall()
+                                }
+                            },
+                        )
+                    }
+                )
             }
         }
         HorizontalDivider()
