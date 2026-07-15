40cc48ceb7f998441ca25bc81a83ee063debe8b1
shub39 <cptnshubham39@gmail.com>
2026-07-15 13:32:56 +0530

    :wrench: chore: 65

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -23,8 +23,8 @@ plugins {
 }
 
 val appName = "Rush"
-val appVersionName = "6.4.3"
-val appVersionCode = 6430
+val appVersionName = "6.5.0"
+val appVersionCode = 6500
 
 val gitHash = execute("git", "rev-parse", "HEAD").take(7)
 
