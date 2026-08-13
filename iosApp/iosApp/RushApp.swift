be9f1907a768cc44943c91f5e77f2885bb829912
shub39 <cptnshubham39@gmail.com>
2026-08-13 07:00:20 +0530

    :wrench: chore: changed project name

---
+++ iosApp/iosApp/RushApp.swift
@@ -0,0 +1,15 @@
+//
+//
+//  Created by Shubham Gorai on 12/08/26.
+//
+
+import SwiftUI
+
+@main
+struct RushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
