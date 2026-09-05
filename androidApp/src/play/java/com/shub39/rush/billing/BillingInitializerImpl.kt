2d5b23acaba2166dde5a3ad65b393d8c4abf02cc
shub39 <cptnshubham39@gmail.com>
2026-09-05 08:51:51 +0530

    :wrench: fix: fixed ANRs

--- androidApp/src/play/java/com/shub39/rush/billing/BillingInitializerImpl.kt
+++ androidApp/src/play/java/com/shub39/rush/billing/BillingInitializerImpl.kt
@@ -21,11 +21,16 @@ import com.revenuecat.purchases.LogLevel
 import com.revenuecat.purchases.Purchases
 import com.revenuecat.purchases.PurchasesConfiguration
 import com.shub39.rush.BuildConfig
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class BillingInitializerImpl : BillingInitializer {
     override fun initialize(context: Context) {
-        Purchases.logLevel = if (BuildConfig.DEBUG) LogLevel.DEBUG else LogLevel.WARN
-        Purchases.configure(PurchasesConfiguration.Builder(context, PURCHASES_KEY).build())
+        CoroutineScope(Dispatchers.IO).launch {
+            Purchases.logLevel = if (BuildConfig.DEBUG) LogLevel.DEBUG else LogLevel.WARN
+            Purchases.configure(PurchasesConfiguration.Builder(context, PURCHASES_KEY).build())
+        }
     }
 
     companion object {
