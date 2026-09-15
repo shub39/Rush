9e27b00f060a07363d1087769a7ed667375a1438
shub39 <cptnshubham39@gmail.com>
2026-09-15 18:06:48 +0530

    :memo: chore: bumped version and added changelog

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -41,8 +41,8 @@ plugins {
 }
 
 val appName = "Rush"
-val appVersionName = "7.0.3"
-val appVersionCode = 7030
+val appVersionName = "7.0.4"
+val appVersionCode = 7040
 
 val gitHash = execute("git", "rev-parse", "HEAD").take(7)
 
